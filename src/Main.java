0c1578c668b3615e124c4adcd601caf8d180968a
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-09-26 18:45:24 -0400

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import br.com.gustavo.desafio.dominio.Curso;
+import br.com.gustavo.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso("Curso Java", "descicao curso java", 8);
+        Curso curso2 = new Curso("Curso Python", "descicao curso python", 6);
+
+        Mentoria mentoria1 = new Mentoria("Mentoria de Java", "descricao mentoria java", LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
     }
 }
